a9353c50f35eb0b86d36a9b6a818fd9150ee251b
Jeon Jin Heo <60263249+zinirun@users.noreply.github.com>
2021-10-03 19:45:43 +0900

    add DynamoDB client

---
+++ db/dynamodb.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
+
+func GetDynamoDBClient() *dynamodb.Client {
+	// Using the SDK's default configuration, loading additional config
+	// and credentials values from the environment variables, shared
+	// credentials, and shared configuration files
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("ap-northeast-2"))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	// Using the Config value, create the DynamoDB client
+	svc := dynamodb.NewFromConfig(cfg)
+	return svc
+}
