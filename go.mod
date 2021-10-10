8c96e417ae07ec49bf5bc3b1539617edecc33431
Jeon Jin Heo <60263249+zinirun@users.noreply.github.com>
2021-10-10 22:14:47 +0900

    add db methods

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/aws/aws-sdk-go-v2 v1.9.1 // indirect
 	github.com/aws/aws-sdk-go-v2/config v1.8.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.5.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,27 +3,25 @@ package main
 import (
 	"context"
 	"fmt"
+	"go-dynamodb/db"
 	"log"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/pkg/errors"
 )
 
+type DBConnection dynamodb.Client
+
 func main() {
-	// Using the SDK's default configuration, loading additional config
-	// and credentials values from the environment variables, shared
-	// credentials, and shared configuration files
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-west-2"))
-	if err != nil {
-		log.Fatalf("unable to load SDK config, %v", err)
-	}
+	ListTables()
+}
 
-	// Using the Config value, create the DynamoDB client
-	svc := dynamodb.NewFromConfig(cfg)
-
-	// Build the request with its input parameters
-	resp, err := svc.ListTables(context.TODO(), &dynamodb.ListTablesInput{
+func ListTables() {
+	svc := db.GetDynamoDBClient()
+	resp, err := (svc).ListTables(context.TODO(), &dynamodb.ListTablesInput{
 		Limit: aws.Int32(5),
 	})
 	if err != nil {
@@ -34,4 +32,64 @@ func main() {
 	for _, tableName := range resp.TableNames {
 		fmt.Println(tableName)
 	}
+}
+
+func CreateTable() {
+	svc := db.GetDynamoDBClient()
+	out, err := svc.CreateTable(context.TODO(), &dynamodb.CreateTableInput{
+		AttributeDefinitions: []types.AttributeDefinition{
+			{
+				AttributeName: aws.String("id"),
+				AttributeType: types.ScalarAttributeTypeS,
+			},
+			{
+				AttributeName: aws.String("createdAt"),
+				AttributeType: types.ScalarAttributeTypeS,
+			},
+		},
+		KeySchema: []types.KeySchemaElement{
+			{
+				AttributeName: aws.String("id"),
+				KeyType:       types.KeyTypeHash,
+			},
+			{
+				AttributeName: aws.String("createdAt"),
+				KeyType:       types.KeyTypeRange,
+			},
+		},
+		TableName:   aws.String("test-table"),
+		BillingMode: types.BillingModePayPerRequest,
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(out)
+}
+
+func WaitForTable(ctx context.Context, db *dynamodb.Client, tableName string) error {
+	w := dynamodb.NewTableExistsWaiter(db)
+	err := w.Wait(ctx,
+		&dynamodb.DescribeTableInput{
+			TableName: aws.String(tableName),
+		},
+		2*time.Minute,
+		func(o *dynamodb.TableExistsWaiterOptions) {
+			o.MaxDelay = 5 * time.Second
+			o.MinDelay = 5 * time.Second
+		})
+	if err != nil {
+		return errors.Wrap(err, "timed out while waiting for table to become active")
+	}
+	return nil
+}
+
+func DeleteTable(tableName string) {
+	svc := db.GetDynamoDBClient()
+	out, err := svc.DeleteTable(context.TODO(), &dynamodb.DeleteTableInput{
+		TableName: aws.String(tableName),
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(out)
 }
