472faf573de7ad34b3feb81eaa9dfa2cedeeab8b
Jeon Jin Heo <60263249+zinirun@users.noreply.github.com>
2021-10-10 23:17:14 +0900

    create dbmanager lib

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module go-dynamodb
 go 1.16
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.9.1 // indirect
-	github.com/aws/aws-sdk-go-v2/config v1.8.2 // indirect
-	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.5.1 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.9.1
+	github.com/aws/aws-sdk-go-v2/config v1.8.2
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.5.1
+	github.com/pkg/errors v0.9.1
 )
---
+++ lib/dbmanager.go
@@ -0,0 +1,96 @@
+package dbmanager
+
+import (
+	"context"
+	"fmt"
+	"go-dynamodb/db"
+	"log"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/pkg/errors"
+)
+
+var client *dynamodb.Client
+
+func init() {
+	client = db.GetDynamoDBClient()
+}
+
+// list all tables
+func ListTables() {
+	resp, err := client.ListTables(context.TODO(), &dynamodb.ListTablesInput{
+		Limit: aws.Int32(5),
+	})
+	if err != nil {
+		log.Fatalf("failed to list tables, %v", err)
+	}
+
+	fmt.Println("Tables:")
+	for _, tableName := range resp.TableNames {
+		fmt.Println(tableName)
+	}
+}
+
+// create a table
+func CreateTable() {
+	out, err := client.CreateTable(context.TODO(), &dynamodb.CreateTableInput{
+		AttributeDefinitions: []types.AttributeDefinition{
+			{
+				AttributeName: aws.String("id"),
+				AttributeType: types.ScalarAttributeTypeS,
+			},
+			{
+				AttributeName: aws.String("createdAt"),
+				AttributeType: types.ScalarAttributeTypeS,
+			},
+		},
+		KeySchema: []types.KeySchemaElement{
+			{
+				AttributeName: aws.String("id"),
+				KeyType:       types.KeyTypeHash,
+			},
+			{
+				AttributeName: aws.String("createdAt"),
+				KeyType:       types.KeyTypeRange,
+			},
+		},
+		TableName:   aws.String("test-table"),
+		BillingMode: types.BillingModePayPerRequest,
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(out)
+}
+
+// wait for table active
+func WaitForTable(ctx context.Context, tableName string) error {
+	w := dynamodb.NewTableExistsWaiter(client)
+	err := w.Wait(ctx,
+		&dynamodb.DescribeTableInput{
+			TableName: aws.String(tableName),
+		},
+		2*time.Minute,
+		func(o *dynamodb.TableExistsWaiterOptions) {
+			o.MaxDelay = 5 * time.Second
+			o.MinDelay = 5 * time.Second
+		})
+	if err != nil {
+		return errors.Wrap(err, "timed out while waiting for table to become active")
+	}
+	return nil
+}
+
+// delete a table by table's name
+func DeleteTable(tableName string) {
+	out, err := client.DeleteTable(context.TODO(), &dynamodb.DeleteTableInput{
+		TableName: aws.String(tableName),
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(out)
+}
